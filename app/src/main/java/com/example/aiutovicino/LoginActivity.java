db04f5eb52e6c7816057179215bdaaf43e8747ce
bordi93 <matteobordignon93@gmail.com>
2022-11-21 19:10:24 +0100

    Push Login
    scheramta login

--- app/src/main/java/com/example/aiutovicino/LoginActivity.java
+++ app/src/main/java/com/example/aiutovicino/LoginActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.aiutovicino.databinding.ActivityLoginBinding;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //setSupportActionBar(binding.appBarLogin.toolbar);
     }
 }
