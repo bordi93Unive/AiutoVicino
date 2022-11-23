ff27aca3997d7b5420d3d5b321de200668138b88
gciviero <giacomo.civiero@gmail.com>
2022-11-23 09:04:51 +0100

    Revert cambio nome
    Cambio nome

---
+++ app/src/main/java/com/example/aiutovicino/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.aiutovicino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aiutovicino.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //setSupportActionBar(binding.appBarLogin.toolbar);
+    }
+}
---
+++ app/src/main/java/com/example/aiutovicino/ui/login/LoginFragment.java
@@ -0,0 +1,59 @@
+package com.example.aiutovicino.ui.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.aiutovicino.MainActivity;
+import com.example.aiutovicino.controller.UserController;
+import com.example.aiutovicino.model.UserModel;
+import com.example.aiutovicino.databinding.FragmentLoginBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+public class LoginFragment extends Fragment {
+
+    private FragmentLoginBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        LoginViewModel loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        binding.buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean login = false;
+                UserModel user = UserController.getUserByEmail(binding.username.getText().toString());
+
+                if(user != null && user.password.equals(binding.password.getText().toString())){
+                    login = true;
+                }
+
+                if(!login){
+                    Snackbar.make(view, "Utente e/o password errati", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+                else{
+                    startActivity(new Intent(getActivity(), MainActivity.class));
+                }
+            }
+        });
+        /*final TextView textView = binding.textHome;
+        loginViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);*/
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
