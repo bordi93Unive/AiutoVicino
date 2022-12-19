7058259e18dbd2a46bd786abe2866e10bd909e32
gciviero <giacomo.civiero@gmail.com>
2022-12-19 21:05:13 +0100

    token in header

--- app/src/main/java/it/unive/aiutovicino/General.java
+++ app/src/main/java/it/unive/aiutovicino/General.java
@@ -62,7 +62,7 @@ public class General {
             URL url = new URL(urlAddress);
             HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
             if(General.user != null) {
-                //conn.addRequestProperty("Authorization", General.user.getToken());
+                conn.addRequestProperty("Token", General.user.getToken());
             }
             conn.setReadTimeout(10000);
             conn.setConnectTimeout(15000);
